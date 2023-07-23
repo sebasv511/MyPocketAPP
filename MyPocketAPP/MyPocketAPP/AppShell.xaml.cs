cc7b024336be5f52d7a90ddf4a0b3b677f69cc47
Sebastian Vallejo Villa <sebasv511@gmail.com>
2023-07-22 21:02:20 -0500

    Initial commit

---
+++ MyPocketAPP/MyPocketAPP/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MyPocketAPP.ViewModels;
+using MyPocketAPP.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyPocketAPP
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ MyPocketAPP/MyPocketAPP/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MyPocketAPP.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MyPocketAPP.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
